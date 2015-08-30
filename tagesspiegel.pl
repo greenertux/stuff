0c028df236441e5c771eb1565e9fa42576ca0bcb
Tristan Hoffmann <github@tristanhoffmann.de>
2015-08-30 22:50:17 +0200

    Bugfixes, added send_from to mail, documentation

--- tagesspiegel.pl
+++ tagesspiegel.pl
@@ -1,3 +1,21 @@
+# Last change: 08/30/2015 by greenertux (Tristan Hoffmann)
+
+# This is a script to fetch the digital version of the German newspaper Tagesspiegel automatically
+# To be used with my script tagesspiegel.sh to send the kindle version directly to your device over WiFi every morning.
+
+# You will need a regular subscription, this script only automates the download process.
+
+# Changes required: 
+# 1) Update customer number and zip code
+
+# Installation
+# Prerequsites: you need perl and perl-CPAN (CentOS: yum install perl-CPAN)
+#		you'll also need the perl modules WWW:Mechanize and File::Slurp 
+#			-> install though package manager of your distribution or try the method below 
+# Run % perl -MCPAN -e shell                    [as root]
+#     > install WWW::Mechanize (had to run yum install perl-libwww-perl perl-XML-TreeBuilder before)
+#     > install File::Slurp
+
 use warnings;
 use strict;
 use WWW::Mechanize;
@@ -9,7 +27,7 @@ $mech->submit_form( fields => { customer_number => '##########', zip_code => '##
 use POSIX; 
 my $date = strftime("%d.%m.%Y", localtime(time));
 print "Downloading Kindle for day " . $date . "\n";
-my $url = 'http://epaper.tagesspiegel.de/epaper/econtent.php?rel=a4' . $date;
+my $url = 'http://e-paper.tagesspiegel.de/epaper/econtent.php?rel=a4' . $date;
 my $file = 'Tagesspiegel-'.$date.'.mobi';
 $mech->get($url);
 File::Slurp::write_file($file, $mech->content);
--- tagesspiegel.sh
+++ tagesspiegel.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-download_path="http://epaper.tagesspiegel.de/epaper/index.php?rel=a4${1:-`date -d "today" +"%d.%m.%Y"`}"
-
 target_file="Tagesspiegel-${1:-`date -d "today" +"%d.%m.%Y"`}.mobi"
 send_to="###@###.###, ###@###.###" #insert your kindle e-mail address(es) here
+send_from="###@###.###" #insert mail-from address here
 
 perl tagesspiegel.pl
 
-echo "Mailtext" | mailx -s "Heutiger Tagesspiegel" -a $target_file $send_to
+echo "Mailtext" | mailx -s "Heutiger Tagesspiegel" -a $target_file -r $send_from $send_to
 
 rm "$target_file"
 
